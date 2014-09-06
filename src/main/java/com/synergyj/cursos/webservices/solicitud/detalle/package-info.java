e50c2707e8e29a2fbb21a64dc129e5a9ddd247ec
edgar bernal remoto <tecualoyaster@gmail.com>
2014-09-06 00:05:33 -0500

    dia 3
    uso de jaxb anotaciones

--- src/main/java/com/synergyj/cursos/webservices/solicitud/detalle/package-info.java
+++ src/main/java/com/synergyj/cursos/webservices/solicitud/detalle/package-info.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantaci—n de la referencia de enlace (JAXB) XML v2.2.7 
 // Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas las modificaciones realizadas en este archivo se perder‡n si se vuelve a compilar el esquema de origen. 
-// Generado el: 2014.08.30 a las 01:55:06 PM CDT 
+// Generado el: 2014.08.30 a las 06:06:09 PM CDT 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://www.synergyj.com/cursos/webservices/solicitud/detalle", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
